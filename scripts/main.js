b0c09c53e7b3a1abb56a4ec97531ab95016250bb
Kakeru Tomioka <tomioka20@c.kyoai.ac.jp>
2022-09-05 17:44:26 +0900

    add table feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,28 @@
 let request = new XMLHttpRequest();
 request.open('GET', 'https://syllabus-cf2b9-default-rtdb.firebaseio.com/.json');
-request.responseType = 'text';
+request.responseType = 'json';
 
 request.onload = function() {
     let response = request.response;
     console.log(response);
+    console.log(typeof(response));
+
+    let src = response.map(function(i) {
+            return '<tr>' +
+                '<td>' + i.subject + '</td>' +
+                '<td>' + i.name + '</td>' +
+                // '<td>' + i.maker + '</td>' +
+                // '<td>' + i.release + '</td>' +
+                // '<td>' + i.price + '</td>' +
+                // '<td>' + i.sales + '</td>' +
+                '</tr>';
+        })
+        .join('');
+    src = '<table>' + src + '</table>';
+    console.log(src);
+
     const res = document.getElementById('result');
-    res.innerHTML = response;
+    res.innerHTML = src;
 };
 
 //request.send();
