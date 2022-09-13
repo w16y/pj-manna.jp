ba34d5fe4b8264e536e5df9e2a750f7d683ca7ea
Kakeru Tomioka <tomioka20@c.kyoai.ac.jp>
2022-09-13 20:15:07 +0900

    add UI & json data

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,43 @@
-let request = new XMLHttpRequest();
-request.open('GET', 'https://syllabus-cf2b9-default-rtdb.firebaseio.com/.json');
-request.responseType = 'json';
+function hoge() {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://syllabus-cf2b9-default-rtdb.firebaseio.com/.json');
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        let response = request.response;
+        console.log(response);
+        console.log(typeof(response));
 
-request.onload = function() {
-    let response = request.response;
-    console.log(response);
-    console.log(typeof(response));
+        let src = response.map(function(i) {
+                return '<tr>' +
+                    '<td>' + i.subject + '</td>' +
+                    '<td>' + i.name + '</td>' +
+                    '<td>' + i.credit + '</td>' +
+                    '<td>' + i.textbook_flg + '</td>' +
+                    '<td>' + '<a href="' + i.sylabus_link + '">' + i.sylabus_link + ' </a></td> ' +
+                    // '<td>' + i.sales + '</td>' +
+                    '</tr>';
+            })
+            .join('');
+        src = '<table class="table table-bordered">' + src + '</table>';
+        console.log(src);
 
-    let src = response.map(function(i) {
-            return '<tr>' +
-                '<td>' + i.subject + '</td>' +
-                '<td>' + i.name + '</td>' +
-                '<td>' + i.credit + '</td>' +
-                '<td>' + i.textbook_flg + '</td>' +
-                '<td>' + '<a href="' + i.sylabus_link + '">' + i.sylabus_link + ' </a></td> ' +
-                // '<td>' + i.sales + '</td>' +
-                '</tr>';
-        })
-        .join('');
-    src = '<table class="table table-bordered">' + src + '</table>';
-    console.log(src);
+        const res = document.getElementById('result');
+        res.innerHTML = src;
+    };
+}
 
-    const res = document.getElementById('result');
-    res.innerHTML = src;
-};
+function getWeekChekbox() {
+    const arr = [];
+    const chk1 = document.form_week.chk1;
+    for (let i = 0; i < chk1.length; i++) {
+        if (chk1[i].checked) {
+            arr.push(chk1[i].value);
+        }
+    }
+    return arr;
+}
+
+
 
 //request.send();
