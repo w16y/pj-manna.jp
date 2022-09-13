6ddf990ca8bc1405a7d0495da6a81edf14c5164e
Kakeru Tomioka <tomioka20@c.kyoai.ac.jp>
2022-09-13 21:50:03 +0900

    名前のプルダウン完了

--- subject-search/scripts/main.js
+++ subject-search/scripts/main.js
@@ -3,6 +3,10 @@ const hoge = async() => {
     const data = await fetch(url).then(res => res.json());
     console.log(data);
     week_arr = getWeekChekbox();
+    console.log(week_arr);
+    credit_arr = getCreditChekbox();
+    console.log(credit_arr);
+
     let src = data.map(function(i) {
             for (let j = 0; j < week_arr.length; j++) {
                 if (i.week === week_arr[j]) {
@@ -29,9 +33,24 @@ const hoge = async() => {
     res.innerHTML = src;
 };
 
+//window.addEventListener('DOMContentLoaded', () => {
+//hoge();
+//});
+
 function getWeekChekbox() {
     const arr = [];
     const chk1 = document.form_week.chk1;
+    for (let i = 0; i < chk1.length; i++) {
+        if (chk1[i].checked) {
+            arr.push(chk1[i].value);
+        }
+    }
+    return arr;
+}
+
+function getCreditChekbox() {
+    const arr = [];
+    const chk1 = document.form_credit.chk1;
     for (let i = 0; i < chk1.length; i++) {
         if (chk1[i].checked) {
             arr.push(chk1[i].value);
