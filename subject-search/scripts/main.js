73a501a6deb023ab59a50443a55b3374e0fcd0c6
Kakeru Tomioka <tomioka20@c.kyoai.ac.jp>
2022-09-14 11:56:42 +0900

    値の取得　完了

--- subject-search/scripts/main.js
+++ subject-search/scripts/main.js
@@ -1,11 +1,20 @@
 const hoge = async() => {
     const url = "data.json";
     const data = await fetch(url).then(res => res.json());
-    console.log(data);
+    const text = document.getElementById('input_text').value;
+    console.log(text);
+    const nameSelect = document.getElementById('t_name').value;
+    console.log(nameSelect);
     week_arr = getWeekChekbox();
     console.log(week_arr);
+    priod_arr = getPeriodChekbox();
+    console.log(priod_arr);
+    semester_arr = getSemesterChekbox();
+    console.log(semester_arr);
     credit_arr = getCreditChekbox();
     console.log(credit_arr);
+    textbookflg_arr = getTextbookFlgSwiches();
+    console.log(textbookflg_arr);
 
     let src = data.map(function(i) {
             for (let j = 0; j < week_arr.length; j++) {
@@ -37,9 +46,41 @@ const hoge = async() => {
 //hoge();
 //});
 
+function getSubjectForm() {
+    const textbox = document.getElementById("input-text");
+    const inputValue = textbox.value;
+}
+
+function getNameForm() {
+    let nameSelect = document.getElementById('t_name');
+    nameSelect.options[2].selected = true;
+}
+
 function getWeekChekbox() {
     const arr = [];
     const chk1 = document.form_week.chk1;
+    for (let i = 0; i < chk1.length; i++) {
+        if (chk1[i].checked) {
+            arr.push(chk1[i].value);
+        }
+    }
+    return arr;
+}
+
+function getPeriodChekbox() {
+    const arr = [];
+    const chk1 = document.form_period.chk1;
+    for (let i = 0; i < chk1.length; i++) {
+        if (chk1[i].checked) {
+            arr.push(chk1[i].value);
+        }
+    }
+    return arr;
+}
+
+function getSemesterChekbox() {
+    const arr = [];
+    const chk1 = document.form_semester.chk1;
     for (let i = 0; i < chk1.length; i++) {
         if (chk1[i].checked) {
             arr.push(chk1[i].value);
@@ -59,6 +100,16 @@ function getCreditChekbox() {
     return arr;
 }
 
+function getTextbookFlgSwiches() {
+    const arr = [];
+    const chk1 = document.form_textbook_flg.chk1;
+    for (let i = 0; i < chk1.length; i++) {
+        if (chk1[i].checked) {
+            arr.push(chk1[i].value);
+        }
+    }
+    return arr;
+}
 
 
 
