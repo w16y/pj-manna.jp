9c69a76caaebb53862fd0ea3d0dbd27ab435a466
Kakeru Tomioka <tomioka20@c.kyoai.ac.jp>
2022-09-15 00:14:03 +0900

    教員名の情報取得　完了

--- subject-search/scripts/main.js
+++ subject-search/scripts/main.js
@@ -5,17 +5,17 @@ const initialize = async() => {
 };
 
 function draw(data) {
-    const week_list = ['','月','火','水','木','金'];
+    const week_list = ['', '月', '火', '水', '木', '金'];
     let src = data.map(function(i) {
         return '<tr>' +
-        '<td>' + week_list[i.week] + '</td>' +
-        '<td>' + i.period + '</td>' +
-        '<td>' + i.subject + '</td>' +
-        '<td>' + i.name + '</td>' +
-        '<td>' + i.credit + '</td>' +
-        '<td>' + i.textbook_flg + '</td>' +
-        '<td>' + '<a href="' + i.sylabus_link + '">' + i.sylabus_link + ' </a></td> ' +
-        '</tr>';
+            '<td>' + week_list[i.week] + '</td>' +
+            '<td>' + i.period + '</td>' +
+            '<td>' + i.subject + '</td>' +
+            '<td>' + i.name + '</td>' +
+            '<td>' + i.credit + '</td>' +
+            '<td>' + i.textbook_flg + '</td>' +
+            '<td>' + '<a href="' + i.sylabus_link + '">' + i.sylabus_link + ' </a></td> ' +
+            '</tr>';
     }).join('');
     src = '<table class="table table-bordered">' +
         '<th>曜日</th>' +
@@ -35,8 +35,11 @@ const dataFilter = async() => {
     const data = await fetch(url).then(res => res.json());
     const text = document.getElementById('input_text').value;
     console.log(text);
-    const nameSelect = document.getElementById('t_name').value;
-    console.log(nameSelect);
+    let nameSelect = document.getElementById('t_name');
+    let idx = nameSelect.selectedIndex;
+    let txt = nameSelect.options[idx].text;
+    console.log(`${txt}`);
+    //const nameSelect = document.getElementById('t_name').value;
     let week_list = getWeekCheckbox();
     console.log(week_list);
     let period_list = getPeriodCheckbox();
@@ -50,23 +53,23 @@ const dataFilter = async() => {
 
     let filtered_data = data;
 
-    if(week_list > 0){
-        for(let i = 0; i < week_list.length; i++){
+    if (week_list > 0) {
+        for (let i = 0; i < week_list.length; i++) {
             filtered_data = filtered_data.filter(d => d.week === week_list[i]);
         }
     }
-    if(period_list > 0){
-        for(let i = 0; i < period_list.length; i++){
+    if (period_list > 0) {
+        for (let i = 0; i < period_list.length; i++) {
             filtered_data = filtered_data.filter(d => d.period === period_list[i]);
         }
     }
-    if(semester_list > 0){
-        for(let i = 0; i < semester_list.length; i++){
+    if (semester_list > 0) {
+        for (let i = 0; i < semester_list.length; i++) {
             filtered_data = filtered_data.filter(d => d.semester === semester_list[i]);
         }
     }
-    if(credit_list > 0){
-        for(let i = 0; i < credit_list.length; i++){
+    if (credit_list > 0) {
+        for (let i = 0; i < credit_list.length; i++) {
             filtered_data = filtered_data.filter(d => d.credit === credit_list[i]);
         }
     }
